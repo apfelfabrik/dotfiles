c2706aafecbefb9962170f41ae3a2966ce590640
Martin Czuchra <martin.czuchra@googlemail.com>
2023-03-11 00:16:27 +0100

    Better insall script
    Handle folders properly in preparation of ~/.config support.

--- install.sh
+++ install.sh
@@ -1,14 +1,38 @@
 #!/bin/bash
+set +e
 
 # init and update submodules for vim
 git submodule init && git submodule update
 
 # symlink dotfiles into ~
 echo 'Symlinking dotfiles...'
-for f in `ls | grep ^[^A-Z\.] | grep -v install`
-do
-  echo -n '~/.'$f
-  ln -is `pwd`/$f ~/.$f
-  echo ' 🥽'
+
+exceptions=("README" "Eclipse" "Resources" "config" "install.sh")
+
+containsElement () {
+  local e match="$1"
+  shift
+  for e; do [[ "$e" == "$match" ]] && return 0; done
+  return 1
+}
+
+repoPath=$(pwd)
+
+for file in ./* ; do
+  basename=$(basename -- "$file")
+  if [ -e "$file" ] && ! containsElement "$basename" "${exceptions[@]}"; then
+
+    dotfile=~/."$basename"
+    target="$repoPath/$basename"
+    if [ -e "$dotfile" ]; then
+      read -rp "$dotfile already exists. Replace? [y/N]" -n1
+      echo
+      if [[ $REPLY =~ ^[Yy]$ ]]; then
+        rm "$dotfile"
+      else
+        continue
+      fi
+    fi
+    ln -isvw "$target" "$dotfile"
+  fi
 done
-echo '🏁 Done!'
